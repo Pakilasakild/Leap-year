48da53f64d1dbc5c77c9a635f3dce49b8538ab48
Domantas <dnasknow@gmail.com>
2024-09-22 17:57:58 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year");
+        int N = sc.nextInt();
+        if (N % 4 == 0 || N % 400 == 0) {
+            System.out.println(N + " is a leap year");
+        }
+        else{
+            System.out.println(N + " is not a leap year");
+        }
+    }
+}
