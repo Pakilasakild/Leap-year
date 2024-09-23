66c621d92a28fcacfcded84701c9594887d7d86a
Domantas <dnasknow@gmail.com>
2024-09-23 15:12:37 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a year");
         int N = sc.nextInt();
-        if (N % 4 == 0 || N % 400 == 0) {
+        if (N % 400 == 0) {
             System.out.println(N + " is a leap year");
         }
-        else{
+        else if (N % 4 == 0 && N % 100 != 0) {
+            System.out.println(N + " is a leap year");
+        }
+        else if (N % 100 == 0) {
             System.out.println(N + " is not a leap year");
         }
     }
